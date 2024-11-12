3963ec951997dadfac4c0fc4f2c933bf51f7cc79
JoseTorres <jtorresjimenez2002@gmail.com>
2024-11-12 13:49:55 -0600

    cors6

--- controlactivos/src/main.ts
+++ controlactivos/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://frontend-ctp-s3ev.vercel.app/', // URL de tu frontend
     methods: ['GET', 'HEAD','PATCH', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
   });
 
