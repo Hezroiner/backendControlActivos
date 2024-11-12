cea68d1c9b148786e3b0651ac3745debcb006595
Hezron <hezroncr@gmail.com>
2024-11-12 15:07:33 -0600

    FRONT VARIABLE

--- controlactivos/src/main.ts
+++ controlactivos/src/main.ts
@@ -5,14 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuración CORS
+  // Configuración de CORS
   app.enableCors({
-    origin: 'https://frontend-ctp-s3ev.vercel.app', // URL del frontend
-    methods: ['GET', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    origin: [
+      'https://frontend-ctp-s3ev.vercel.app',
+      'http://localhost:5173',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Authorization'],
     credentials: true,
-    optionsSuccessStatus: 200,
   });
 
   const port = process.env.PORT || 3000;
