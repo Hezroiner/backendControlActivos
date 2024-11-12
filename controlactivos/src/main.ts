d29dc8c244f5cadd04261148a2cac3ebe7c9e85b
JoseTorres <jtorresjimenez2002@gmail.com>
2024-11-12 13:33:42 -0600

    cors2

--- controlactivos/src/main.ts
+++ controlactivos/src/main.ts
@@ -1,19 +1,18 @@
 import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   
 
-  app.use(cors({
-    origin: 'https://frontend-ctp-s3ev.vercel.app', // Cambia esto por la URL de tu frontend
-    methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
-    credentials: true, // Si necesitas enviar cookies o encabezados personalizados
-  }));
+  app.enableCors({
+    origin: 'https://frontend-ctp-s3ev.vercel.app/', // URL de tu frontend
+    methods: ['GET', 'HEAD','PATCH', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Permitir cookies y encabezados personalizados
+  });
+
+
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
