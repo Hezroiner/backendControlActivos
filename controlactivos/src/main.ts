e4a3fed31aedda0d00adb2f353dbea1db2a47ae1
JoseTorres <jtorresjimenez2002@gmail.com>
2024-11-12 13:48:32 -0600

    cors5

--- controlactivos/src/main.ts
+++ controlactivos/src/main.ts
@@ -14,8 +14,7 @@ async function bootstrap() {
 
 
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(3000);
   console.log(`🚀 Backend corriendo en: ${await app.getUrl()}`);
 }
 
