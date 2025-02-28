c1984b2c690e6122ed0b0205f2f64e2b8b026dce
Hezron <hezroncr@gmail.com>
2025-02-28 10:48:01 -0600

    BE mas actualizado

--- controlactivos/src/Entities/activo.entity.ts
+++ controlactivos/src/Entities/activo.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Ubicacion } from "./ubicacion.entity";
 import { Licitacion } from "./licitacion.entity"; // Relación con Licitacion
+import { text } from "stream/consumers";
 
 @Entity()
 export class Activo {
@@ -10,7 +11,7 @@ export class Activo {
     @Column()
     nombre: string;
 
-    @Column()
+    @Column({ type: 'text', nullable : true })
     descripcion: string;
 
     @Column()
@@ -39,14 +40,14 @@ export class Activo {
     @Column()
     numPlaca: string;
 
-    @Column({nullable : true})
+    @Column({ nullable: true })
 
     foto: string;
 
     @Column({ nullable: true })
     precio: number;
 
-    @Column({ nullable: true })
+    @Column({ type : 'text' , nullable: true })
     observacion: string;
 
     @Column()
