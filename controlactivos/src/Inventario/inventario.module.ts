0d80802d54d2f894ce368536365b5edc921a3370
Hezron <hezroncr@gmail.com>
2025-03-15 20:52:57 -0600

    Inventario Module cambiado

--- controlactivos/src/Inventario/inventario.module.ts
+++ controlactivos/src/Inventario/inventario.module.ts
@@ -7,12 +7,15 @@ import { Ubicacion } from '@app/Entities/ubicacion.entity';
 import { Activo } from '@app/Entities/activo.entity';
 import { InventarioService } from './inventario.service';
 import { InventarioController } from './inventario.controller';
+import { AuthModule } from '../auth/auth.module'; // Importa AuthModule para que JwtService esté disponible
+import { RolesGuard } from '../auth/roles.guard'; // Importa RolesGuard
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Inventario, InventarioDetalle, User, Ubicacion, Activo]),
+    forwardRef(() => AuthModule), 
   ],
-  providers: [InventarioService],
+  providers: [InventarioService, RolesGuard], // Agrega RolesGuard a providers
   controllers: [InventarioController],
   exports: [InventarioService],
 })
